6ce82317271004304d5b0abdc0281951e67c2cc4
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-08-04 19:45:52 -0400

    Create StickyNav.jsx
    First use of react for specific-componenet-nav

---
+++ StickyNav.jsx
@@ -0,0 +1,12 @@
+
+export default function Sticky_Nav() {
+    return (
+        <nav>
+        <h1>TazTheProgrammer</h1>
+          <ul className='nav-bar'>
+            <li>About Me</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+    )
+  }
